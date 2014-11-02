7b375feb08b6c20cb54b6eb1d6dfc1afab9eb99d
egdenis <egdenis@gmail.com>
2014-11-02 16:19:09 -0600

    working game, no score, abstracted metaball

--- metaballs.js
+++ metaballs.js
@@ -1,7 +1,12 @@
 var GAME;
 
 window.onload = function(){
+  window.addEventListener('mousemove', onMousemove, false);
+
   GAME = new game();
+  console.log("gsf")
+  GAME.metaball.makePoints(32);
+
   GAME.act();
 }
 
@@ -9,42 +14,48 @@ function game(){
   this.preRenderCanvas = document.createElement("canvas")
   this.ctxPreRender = this.preRenderCanvas.getContext("2d");
   this.ctx = canvas.getContext("2d");
+  canvas.style.cursor = 'none';
+  canvas.style.border = "1px solid black"
 
   
 
-  this.preRenderCanvas.height = canvas.width = 1000;
-  this.preRenderCanvas.width = canvas.height = 1000;
+  this.preRenderCanvas.height = canvas.width = 500;
+  this.preRenderCanvas.width = canvas.height = 500;
 
   this.width = canvas.width;
   this.height = canvas.height;
 
-
-  this.ctx.fillRect(0,0,10,10)
-  this.points = [];
-  this.threshold = 240;
-
-  for (var i = 50; i >= 0; i--) {
-    var x = this.width*Math.random();
-    var y = this.width*Math.random();
-    var vx = Math.random()*10-5;
-    var vy = Math.random()*10-5;
-    var size = Math.random()*10+150;
-    this.points.push({x:x,y:y,vx:vx,vy:vy,size:size});
-  };
-  this.metapoints = [];
-}
+  this.player = new player();
+  this.metaball = new metaballs();
+  }
 
 game.prototype.act = function(){
-  metabolize();
-  processMetaball();
+  GAME.metaball.metabolize();
+  GAME.metaball.processMetaball();
+  GAME.player.act();
   requestAnimFrame(GAME.act);
 }
 
+function metaballs(){
+  this.points = [];
+  this.threshold = 200;
+}
 
-function metabolize(){ 
+metaballs.prototype.makePoints = function(num){
+  for (var i = num; i >= 0; i--) {
+    var x = GAME.width*.5;
+    var y = GAME.width*.5;
+    var vx = Math.random()*6-3;
+    var vy = Math.random()*6-3;
+    var size = Math.random()*100+50;
+    this.points.push({x:x,y:y,vx:vx,vy:vy,size:size});
+  };
+}
+
+metaballs.prototype.metabolize =function(){ 
   GAME.ctxPreRender.clearRect(0,0,GAME.width,GAME.height)
-  for (var i = GAME.points.length - 1; i >= 0; i--) {
-    var point = GAME.points[i];
+  for (var i = this.points.length - 1; i >= 0; i--) {
+    var point = this.points[i];
     point.x += point.vx;
     point.y += point.vy;
 
@@ -63,26 +74,79 @@ function metabolize(){
 
     GAME.ctxPreRender.beginPath();
     var gradient = GAME.ctxPreRender.createRadialGradient(point.x, point.y, 1, point.x, point.y, point.size);
-    gradient.addColorStop(1,"rgba(0,0,0,0)");
-    gradient.addColorStop(0,"green");
+    gradient.addColorStop(1,"rgba(230,0,250,0)");
+    gradient.addColorStop(0,"red");
     GAME.ctxPreRender.fillStyle = gradient;
     GAME.ctxPreRender.arc(point.x, point.y, point.size, 0, Math.PI*2);
     GAME.ctxPreRender.fill();
   };
 }
 
-function processMetaball(){
+metaballs.prototype.processMetaball = function(){
   var frame = GAME.ctxPreRender.getImageData(0,0,GAME.width,GAME.height)
   var pixel = frame.data;
 
   for (var i = 0, n = pixel.length; i <n; i += 4) {
-    if(pixel[i+3]<GAME.threshold){
-       pixel[i+3]=0;
+    if(pixel[i+3]<this.threshold){
+       pixel[i+3]=0;  
+    }
+  }
+
+  var ymax = GAME.player.y+GAME.player.radius; 
+  var xmax = GAME.player.x+GAME.player.radius;
+
+  for (var y = GAME.player.y-GAME.player.radius; y < ymax; y+=3) {
+    for (var x = GAME.player.x-GAME.player.radius; x < xmax; x+=3) {
+      var index = (y * GAME.width + x) * 4;
+      if (pixel[index+3]) {
         
-        
+        GAME.player.color = "rgba(0,0,0,0)"
+
+      };
     }
   }
   GAME.ctx.putImageData(frame, 0, 0);    
+
+}
+
+function player(){
+  this.x = -10;
+  this.y = -10;
+  this.radius = 3;
+  this.color = "blue"
+}
+
+player.prototype.draw = function(){
+  GAME.ctx.fillStyle = this.color
+  GAME.ctx.beginPath();
+  GAME.ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+  GAME.ctx.fill();
+}
+
+player.prototype.act = function(){
+  this.draw();
+}
+
+
+
+function onMousedown(e){
+  var pos = getMousePos(canvas, e);
+
+}
+
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+  }
+
+function onMousemove(e){
+  var pos = getMousePos(canvas, e);
+  GAME.player.x = pos.x;
+  GAME.player.y = pos.y;
 }
 
 window.requestAnimFrame = (function(){
