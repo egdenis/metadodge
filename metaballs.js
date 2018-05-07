0041ba57b97da7e523029f982f823dd04cb2d385
Etienne Denis <egdenis@gmail.com>
2018-05-07 01:44:45 -0400

    Fixed bug, cleaned code a bit, added nice colors

--- metaballs.js
+++ metaballs.js
@@ -2,10 +2,11 @@ var GAME;
 
 window.onload = function(){
   window.addEventListener('mousemove', onMousemove, false);
-  window.addEventListener("mousedown", onMousedown, false);
 
   GAME = new game();
+  console.log("gsf")
   GAME.metaball.makePoints(32);
+
   GAME.act();
 }
 
@@ -13,62 +14,68 @@ function game(){
   this.preRenderCanvas = document.createElement("canvas")
   this.ctxPreRender = this.preRenderCanvas.getContext("2d");
   this.ctx = canvas.getContext("2d");
+  canvas.style.cursor = 'none';
   canvas.style.border = "1px solid black"
+  this.gameover = false;
+  
+this.score = 0
+  this.preRenderCanvas.height = canvas.width = 600;
+  this.preRenderCanvas.width = canvas.height = 600;
 
-  this.width = canvas.width = this.preRenderCanvas.width = 500;
-  this.height = canvas.height = this.preRenderCanvas.height = 500;
-  this.state = "menu";
+  this.width = canvas.width;
+  this.height = canvas.height;
 
-  this.ui = new UI([new Button(this.width/2,this.height/2,100,50,"game","menu",(this.init.bind(this)))]);
+  this.player = new player();
   this.metaball = new metaballs();
-
-
   }
 
 game.prototype.act = function(){
-  if (GAME.state == "game") {
-    GAME.player.act();
+  GAME.metaball.metabolize();
+  GAME.metaball.processMetaball();
+  GAME.player.act();
+  GAME.score +=1
+  if(GAME.score%500 == 0){
+    console.log(GAME.score);
   }
-    GAME.metaball.metabolize();
-    GAME.metaball.processMetaball();
-
-  GAME.ui.drawUI()
-  console.log(this.state);
   requestAnimFrame(GAME.act);
-
-}
-
-game.prototype.init = function(){
-  this.player = new player();
-  this.metaball = new metaballs();
-  this.metaball.makePoints(32);
-    canvas.style.cursor = 'none';
-  this.state = "game"
-
 }
 
 function metaballs(){
   this.points = [];
-  this.threshold = 200;
-}
+  this.threshold = 220;
+  this.MAX_SIZE = 200;
+  this.MAX_SPEED = 4;
+  }
 
 metaballs.prototype.makePoints = function(num){
   for (var i = num; i >= 0; i--) {
     var x = GAME.width*.5;
     var y = GAME.width*.5;
-    var vx = Math.random()*6-3;
-    var vy = Math.random()*6-3;
-    var size = Math.random()*100+50;
-    this.points.push({x:x,y:y,vx:vx,vy:vy,size:size});
+    var vx = Math.random()*this.MAX_SPEED*2 - this.MAX_SPEED;
+    var vy = Math.random()*this.MAX_SPEED*2 - this.MAX_SPEED;
+    var size = Math.random()*this.MAX_SIZE+70;
+    var color2 = randomColor(0);
+    var color1 = randomColor(1);
+
+    this.points.push({x:x,y:y,vx:vx,vy:vy,size:size, color1:color1, color2: color2});
   };
 }
-
+function parseRGBA(rgb){
+    rgb = rgb.substring(4, rgb.length-1)
+             .replace(/ /g, '')
+             .split(',');
+    return rgb
+}
 metaballs.prototype.metabolize =function(){ 
   GAME.ctxPreRender.clearRect(0,0,GAME.width,GAME.height)
   for (var i = this.points.length - 1; i >= 0; i--) {
     var point = this.points[i];
     point.x += point.vx;
     point.y += point.vy;
+
+    //var colors = parseRGBA(color1)
+    //var color2 = parseRGBA(color2)
+    
 
     if(point.x > GAME.width+point.size){
         point.x = 0-point.size;
@@ -85,8 +92,8 @@ metaballs.prototype.metabolize =function(){
 
     GAME.ctxPreRender.beginPath();
     var gradient = GAME.ctxPreRender.createRadialGradient(point.x, point.y, 1, point.x, point.y, point.size);
-    gradient.addColorStop(1,"rgba(230,0,250,0)");
-    gradient.addColorStop(0,"red");
+    gradient.addColorStop(1,point.color2);
+    gradient.addColorStop(0,point.color1);
     GAME.ctxPreRender.fillStyle = gradient;
     GAME.ctxPreRender.arc(point.x, point.y, point.size, 0, Math.PI*2);
     GAME.ctxPreRender.fill();
@@ -103,22 +110,21 @@ metaballs.prototype.processMetaball = function(){
     }
   }
 
-  if (GAME.state == "game") {
-    var ymax = GAME.player.y+GAME.player.radius; 
-    var xmax = GAME.player.x+GAME.player.radius;
+  var ymax = GAME.player.y+GAME.player.radius; 
+  var xmax = GAME.player.x+GAME.player.radius;
 
-    for (var y = GAME.player.y-GAME.player.radius; y < ymax; y+=3) {
-      for (var x = GAME.player.x-GAME.player.radius; x < xmax; x+=3) {
-        var index = (y * GAME.width + x) * 4;
-        if (pixel[index+3]) {
-          
-          GAME.player.color = "rgba(0,0,0,0)"
-
-        };
-      }
+  for (var y = GAME.player.y-GAME.player.radius; y < ymax; y+=3) {
+    for (var x = GAME.player.x-GAME.player.radius; x < xmax; x+=3) {
+      var index = (y * GAME.width + x) * 4;
+      if (pixel[index+3]) {
+        if(GAME.gameover == false)
+          console.log(GAME.score)
+           GAME.player.color = "rgba(0,0,0,0)";
+        GAME.gameover = true;
+        
+      };
     }
   }
-
   GAME.ctx.putImageData(frame, 0, 0);    
 
 }
@@ -141,16 +147,13 @@ player.prototype.act = function(){
   this.draw();
 }
 
-
+function randomColor(alpha){
+  return "rgba(" +Math.floor(Math.random()*255) +","+Math.floor(Math.random()*255) +","+Math.floor(Math.random()*255) +"," + alpha + ")";
+}
 
 function onMousedown(e){
   var pos = getMousePos(canvas, e);
-  for (var i = 0; i < GAME.ui.buttons.length; i++) {
 
-    if (GAME.ui.buttons[i].isClicked(pos.x,pos.y)&&GAME.state == GAME.ui.buttons[i].state) {
-      GAME.ui.buttons[i].callback();
-    };
-  };  
 }
 
 
@@ -164,17 +167,8 @@ function getMousePos(canvas, evt) {
 
 function onMousemove(e){
   var pos = getMousePos(canvas, e);
-  if (GAME.state == "game") {
-    GAME.player.x = pos.x;
-    GAME.player.y = pos.y;
-  }
-  for (var i = GAME.ui.buttons.length - 1; i >= 0; i--) {
-      if (GAME.ui.buttons[i].isClicked(pos.x,pos.y)) {
-        GAME.ui.buttons[i].fillStyle = "rgb(80,80,80)";
-      }
-      else
-        GAME.ui.buttons[i].fillStyle = "rgb(50,50,50)";
-    };
+  GAME.player.x = pos.x;
+  GAME.player.y = pos.y;
 }
 
 window.requestAnimFrame = (function(){
